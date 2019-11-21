76378507aa594387f570ddf5df09122fbf3cac28
pedroivovasconcelos <56645430+pedroivovasconcelos@users.noreply.github.com>
2019-11-21 14:46:18 -0300

    teste3

--- rastreador/main.py
+++ rastreador/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/<name>")
+def undefined_page(name):
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
