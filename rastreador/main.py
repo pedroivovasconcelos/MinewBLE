515278224e263ff388e033cc3f43ef9d08fcc065
pedroivovasconcelos <56645430+pedroivovasconcelos@users.noreply.github.com>
2019-11-21 14:18:12 -0300

    teste1

--- rastreador/main.py
+++ rastreador/main.py
@@ -1,20 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
